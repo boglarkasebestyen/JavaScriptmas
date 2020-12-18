1c0b930e64331fed0aee84967b021b378245595a
Boglárka Sebestyén <22778918+boglarkasebestyen@users.noreply.github.com>
2020-12-17 17:21:52 -0800

    Update Day 13 - ExtracteachKth.js

--- Day 13 - ExtracteachKth.js
+++ Day 13 - ExtracteachKth.js
@@ -9,7 +9,7 @@ Example:
 
 For inputArray = [1,2,3,4,5,6,7,8,9,10] and k = 3, the output should be extractEachkth(inputArray, k) = [1,2,4,5,7,8,10]
 
-Hints: filter() --> The filter() method creates a new array with all elements that pass the test implemented by the provided function.
+Hints: filter() 
 
 */
 
